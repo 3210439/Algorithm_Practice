5a38080c4b4cdb0d1dfbe8efe17e7b27cd3ab3a9
종완 <47379176+3210439@users.noreply.github.com>
2022-01-15 03:54:58 +0000

    주사위 내게 다른 해결방법

--- back/baekjoon/주사위 내게.py
+++ back/baekjoon/주사위 내게.py
@@ -1,6 +1,6 @@
 # 주사위 네개
 #https://www.acmicpc.net/problem/2484
-
+# 나의 풀이
 def solution(lst):
     lst.sort()
     zusa = [0]*7
@@ -40,5 +40,21 @@ for i in range(n):
     answer.append(solution(list(map(int, input().split()))))
 print(max(answer))
                     
-                    
-                
+# 강의 풀이
+def money():
+  lst = sorted(list(map(int, input().split())))
+  if len(set(lst)) == 1:
+    return lst[0] * 5000 + 50000
+  if len(set(lst)) == 2:
+    if lst[1] == lst[2]:
+      return 10000 + lst[1] * 1000
+    else:
+      return 2000 + (lst[1] + lst[2]) * 500
+  for i in range(3):
+    if lst[i] == lst[i+1]:        
+      return 1000 + lst[i] * 100
+  return lst[-1] * 100
+
+N = int(input())
+
+print(max(money() for i in range(N)))
